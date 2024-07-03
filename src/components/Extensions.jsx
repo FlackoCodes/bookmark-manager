c04174566ef068c9279c8d6a2855c9961134f368
Flacko <lesliejoe2k2@gmail.com>
2024-07-03 21:54:16 +0000

    added json file for browser link data

---
+++ src/components/Extensions.jsx
@@ -0,0 +1,20 @@
+const Extensions = () => {
+  return (
+    <>
+    <div className="download">
+        <header>
+            <h5>Download the extension</h5>
+            <p>We’ve got more browsers in the pipeline. Please do let us know if you’ve 
+            got a favourite you’d like us to prioritize.</p>
+        </header>
+        <div className="browser-links">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+    </>
+  )
+}
+ 
+export default Extensions
